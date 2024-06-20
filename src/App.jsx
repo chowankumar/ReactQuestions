e3344989246b3c55de52eca8ca4ed7458a8868ba
chowankumar <chowanmalhani5@gmail.com>
2024-06-20 12:55:02 +0500

    tQuestion one done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
  import React from 'react'
- 
+ import One from './One.jsx'
  export const App = () => {
    return (
      <div>
-       
+       <One/>
      </div>
    )
  }
---
+++ src/One.jsx
@@ -0,0 +1,9 @@
+ import React from 'react'
+ 
+ const One = () => {
+   return (
+     <div>this is the question number one</div>
+   )
+ }
+ 
+ export default One
