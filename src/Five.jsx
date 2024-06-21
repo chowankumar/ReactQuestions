8ed150440ada1f74960c6dd59501715ed76a03b9
chowankumar <chowanmalhani5@gmail.com>
2024-06-21 21:21:42 +0500

    add note in the question five

--- src/Five.jsx
+++ src/Five.jsx
@@ -16,3 +16,6 @@ const Five = () => {
 }
 
 export default Five
+
+// Use onChange: For input elements like checkboxes, radio buttons, and text inputs, because it accurately reflects changes to the input's value.
+// Use onClick: For button elements or when you need to respond to any click event, not necessarily tied to an input value change.
